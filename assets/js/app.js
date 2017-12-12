8e6ff42cfb98f45633c0b2c22aef9ce7d9ea084b
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-11 23:30:18 -0600

    User input and submit functioning. On click printint rating and URL to page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,69 @@
-$(document).ready(function(){
-				 
-	var killersGifs = {
-		
-		killerSearches: ["Freddy", "Jason", "Pennywise", "Ghost Face", "American Mary", "Chucky", "Bride of Chucky", "Candyman", "Cujo", "Hannibal", "Leatherface", "Michael Meyers", "Norman Bates", "Pinhead"],
-		
-		buttonLoop: function() {
-			for (var i = 0; i < killersGifs.killerSearches.length - 1; i++) {
-			var buttonM = $("<button class='dynGen'>" + killersGifs.killerSearches[i] + "</button>").click(killersGifs.divLoop);
-			$("#buttons").append(buttonM);
-			}
-		},
-		
-		divLoop: function() {
-			for (var i = 0; i < killersGifs.killerSearches.length - 1; i++) {
-				//console.log(this.killerSearches[i]);
-				var newDiv = $("<div class='gif-div'>");
-				var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=B26sstJns2pZuNT5HiJpqS5FV8Su1sDd&q=" + killer + "&limit=10"
-				var killer = killersGifs.killerSearches[i];
-				//console.log(queryURL);
-				$.ajax({
-					url: queryURL,
-					method: "GET"
-				}).done(function(response) {
-				   console.log(response);
-					for (var x = 0; x < response.length - 1; x++) {
-						var respData = response.data[x];
-						var image = respData.images.fixed_height_small_still.url;
-						var killerImg = $("<img>");
-						killerImg.attr("src", image);
-						killerImg.attr("alt", "Serial Killer still frame of gif");
-						$("#append-img-div")
-						$("#append-img-div").append(killerImg);
-					
-					};
-				
-				});
-				
-			};
-		}
-	};
-	
-	killersGifs.buttonLoop();
-	
-	
-	
-	
-	
+var killersGifs = {
+
+  killerSearches: ["Freddy", "Jason", "Pennywise", "Ghost Face", "American Mary", "Chucky", "Bride of Chucky", "Candyman", "Cujo", "Hannibal", "Leatherface", "Michael Meyers", "Norman Bates", "Pinhead"],
+
+  buttonLoop: function() {
+    for (var b = 0; b < killersGifs.killerSearches.length - 1; b++) {
+      var buttonM = $("<button class='dynGen'>").text(killersGifs.killerSearches[b]).attr("data-index", killersGifs.killerSearches[b]);
+      $("#buttons").append(buttonM);
+    }
+  },
+
+  divLoop: function(event) {
+    for (var i = 0; i < killersGifs.killerSearches.length - 1; i++) {
+      //console.log(this.killerSearches[i]);
+      //var newDiv = $("<div class='gif-div'>");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=B26sstJns2pZuNT5HiJpqS5FV8Su1sDd&q=" + killer + "&limit=10"
+
+      var killer = killersGifs.killerSearches[i];
+      //console.log(queryURL);
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+        console.log(response.data.length);
+        for (var x = 0; x < response.data.length - 1; x++) {
+          var respData = response.data[x];
+          var image = respData.images.fixed_height_small_still.url;
+          var rating = respData.rating;
+
+          var dynDiv = $("<div class='dyn-div'></div>");
+          var killerImg = $("<img>");
+
+          killerImg.attr("src", image);
+          killerImg.attr("alt", "Serial Killer still frame of gif");
+
+          dynDiv.append("Rating: " + rating);
+          dynDiv.append(image);
+
+
+          $("#append-img-div").prepend($(dynDiv));
+
+        };
+
+      });
+
+    };
+  },
+
+  userPush: function() {
+    var userInput = $("input[type='text']").val().trim();
+    console.log(userInput);
+    killersGifs.killerSearches.push(userInput);
+    console.log(killersGifs.killerSearches);
+  }
+
+};
+
+killersGifs.buttonLoop();
+
+$("#killer-add-submit").on("click", function(event) {
+  event.preventDefault();
+  killersGifs.userPush();
 });
-//CODE TO CREATE STILL IMAGE DIVS
+
+$(document).on("click", "button.dynGen", function(event) {
+  event.preventDefault();
+  $("#append-img-div").empty();
+  killersGifs.divLoop(event);
+});
