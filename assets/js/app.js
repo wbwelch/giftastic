6580434dc0a8dd1ec260cd7c4379a5a5a42c0216
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-11 20:45:41 -0600

    Array added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 				 
-	
+	var killersGifs = {
+		killerSearches: ["Freddy", "Jason", "Pennywise", "Ghost Face", "American Mary", "Chucky", "Bride of Chucky", "Candyman", "Cujo", "Hannibal", "Leatherface", "Michael Meyers", "Norman Bates", "Pinhead"]
+	}
 	
 	
 });
