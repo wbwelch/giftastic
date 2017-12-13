f1439d0b171f612bcb4934e92f7d35b370845a1e
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2017-12-13 17:57:31 -0600

    On click toggles between gif and image. Styling complete.'

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ lastClick = [];
 	
 var killersGifs = {
 
-  killerSearches: ["Freddy Krueger", "Jason Voorhees", "Pennywise", "Ghostface", "American Mary", "Chucky", "Bride of Chucky", "The Candyman", "Cujo", "Hannibal", "Leatherface", "Michael Meyers", "Norman Bates", "Pinhead"],
+  killerSearches: ["Freddy Krueger", "Jason Voorhees", "Pennywise", "Ghostface", "American Mary", "Chucky", "Bride of Chucky", "The Candyman", "Cujo", "Hannibal", "Leatherface", "Michael Myers", "Norman Bates", "Pinhead"],
 
   buttonLoop: function() {
     for (var b = 0; b < killersGifs.killerSearches.length - 1; b++) {
@@ -15,58 +15,48 @@ var killersGifs = {
   },
 
   divLoop: function(click) {
-    //for (var i = 0; i < killersGifs.killerSearches.length - 1; i++) {
-      //console.log(this.killerSearches[i]);
-	  //var killer = killersGifs.killerSearches[i];
+
       var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=B26sstJns2pZuNT5HiJpqS5FV8Su1sDd&q=" + lastClick + "&limit=10"
 
-     
-      //console.log(queryURL);
       $.ajax({
         url: queryURL,
         method: "GET"
       }).done(function(response) {
-        console.log(response.data);
-        for (var x = 0; x < response.data.length - 1; x++) {
-          var respData = response.data[x];
-          var image = respData.images.fixed_height_small_still.url;
-		  var gif = respData.images.fixed_height_small.url;
-          var rating = respData.rating;
 
-          var dynDiv = $("<div class='dyn-div'>");
-		  dynDiv.attr("data-index", response.data[x]);
-	
-          var killerImg = $("<img class='still-image'>");
-
-          killerImg.attr("src", image);
-          killerImg.attr("alt", "Serial Killer still frame of gif");
-		 
-
-          dynDiv.append("<p> Rating: " + rating + "</p>");
-          dynDiv.append(killerImg);
-
-
-          $("#append-img-div").prepend($(dynDiv));
-		
 		  
+        for (var i = 0; i < response.data.length; i++) {
+        	var respData = response.data[i];
+        	var image = respData.images.fixed_height_still.url;
+			var gif = respData.images.fixed_height.url;
+        	var rating = respData.rating;
 
+        	var dynDiv = $("<div class='dyn-div'>");
+
+        	var killerImg = $("<img class='still-image'>");
+
+        	killerImg.attr("src", image);
+        	killerImg.attr("alt", "Serial Killer still frame of gif");
+			killerImg.attr("data-gif", gif);
+			killerImg.attr("class", "killerImg");
+			killerImg.attr("data-index", i);
+			killerImg.attr("data-img", image);
+
+        	dynDiv.append("<p> Rating: " + rating + "</p>");
+        	dynDiv.append(killerImg);
+
+        	$("#append-img-div").prepend($(dynDiv));
 			};
 
       	});
-
-	
-  },
-  
-
-  userPush: function () {
-        var userInput = $("input[type='text']").val().trim();
-        console.log(userInput);
+  	},
+	userPush: function () {
+     	var userInput = $("input[type='text']").val().trim();
+        //console.log(userInput);
         killersGifs.killerSearches.push(userInput);
         var buttonU = $("<button class='dynGen'>").text(userInput).attr("data-index", userInput);
         $("#buttons").append(buttonU);
-        console.log(killersGifs.killerSearches);
-  }
-
+        //console.log(killersGifs.killerSearches);
+  	}
 };
 
 killersGifs.buttonLoop();
@@ -79,13 +69,28 @@ $("#killer-add-submit").on("click", function(event) {
 $(document).on("click", "button.dynGen", function(event) {
   	var currentIndex = $(this).attr("data-index");
 	lastClick.push(currentIndex);
-	console.log(currentIndex);
+	//console.log(currentIndex);
 	event.preventDefault();
 	$("#append-img-div").empty();
 	killersGifs.divLoop();
 	lastClick = [];
 });
 	
+$(document).on("click", ".killerImg", function(event) {
+	//console.log("test");
+	var currentIn = $(this).attr("data-index");
+	var tempUrl = $(this).attr("data-gif");
+	var tempUrl2 = $(this).attr("data-img");
+	//console.log(currentIn);
+	//console.log(tempUrl);
+	if ($(this).attr("src") == tempUrl2) {
+
+	$(this).attr("src", tempUrl);
+	}
+	else if ($(this).attr("src") == tempUrl) {
+	$(this).attr("src", tempUrl2);
+	};
+});
 
 		
 });
